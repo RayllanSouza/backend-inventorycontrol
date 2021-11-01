312a16261658518ad3ddca17564ee0a48763e8c8
RayllanSouza <rayllan51@gmail.com>
2021-11-01 00:16:50 -0300

    Created Log Sistem

--- src/Controllers/crud.controllers.js
+++ src/Controllers/crud.controllers.js
@@ -18,18 +18,61 @@ function showItems(req, res) {
 }
 
 function removeItem(req, res) {
-    const {id} = req.body;
-    connection.query(`DELETE FROM items WHERE id=${id}`,function (error, result){
-        if(result.affectedRows === 0){
+    const {id, qtde, user} = req.body;    
+    connection.query(`SELECT * FROM items WHERE id=${id}`, function (error, result){
+        if(result.length <= 0){
             return res.status(500).json({
-                "Error": "Server Internal Error"
+                "Error": "Item nao encontrado!"
             })
         }else{
-            return res.status(200).json({
-                "Status": "Sucess"
-            })
+            const nomeItem = result[0].nome;
+            const qtdeBD = result[0].qtde;
+            const qtdeRemove = parseInt(qtdeBD) - parseInt(qtde);
+            if(qtdeRemove < 0){
+                return res.status(400).json({
+                    "Error": "Voce nao pode retirar essa quantidade, pois nao há no estoque."
+                })
+            }else if(qtdeRemove === 0){
+                connection.query(`DELETE FROM items WHERE id=${id}`,function (error, result){
+                    if(result.affectedRows === 0){
+                        return res.status(500).json({
+                            "Error": "Server Internal Error"
+                        })
+                    }else{
+                        const log = {
+                            Acao: `Removeu ${qtde} de ${nomeItem}`,
+                            Agente: user
+                        }
+                        connection.query(`INSERT INTO logs SET ?`, log ,function (error, result){
+                            return res.status(200).json({
+                                "Status": "Item Removido"
+                            })
+                        })
+                    }
+                })    
+            }else{
+                connection.query(`UPDATE items SET qtde = '${qtdeRemove}' WHERE id = '${result[0].id}'`,function (error, result){
+                    if(result.affectedRows === 0){
+                        return res.status(500).json({
+                            "Error": "Não foi possivel atualizar item!"
+                        })
+                    }else{
+                        const log = {
+                            Acao: `Removeu ${qtde} de ${nomeItem}`,
+                            Agente: user
+                        }
+                        connection.query(`INSERT INTO logs SET ?`, log ,function (error, result){
+                            return res.status(200).json({
+                                "Status": "Qtde Atualizada!",
+                                "newQtde": qtdeRemove
+                            })
+                        })
+                    }
+                })    
+            }
         }
-    })    
+    })
+
 }
 
 function addItem(req, res){
@@ -39,11 +82,11 @@ function addItem(req, res){
             connection.query(`INSERT INTO items SET ?`, item ,function (error, result){
                 if(result.affectedRows === 0){
                     return res.status(500).json({
-                        "Error": "Server Internal Error"
+                        "Error": "Não foi possivel adicionar item!"
                     })
                 }else{
                     return res.status(200).json({
-                        "Status": "Sucess"
+                        "Status": "Item Adicionado!"
                     })
                 }
             })    
@@ -53,7 +96,7 @@ function addItem(req, res){
             connection.query(`UPDATE items SET qtde = '${qtdeUpdate}' WHERE id = '${result[0].id}'`, function(error, result){
                 if(result.affectedRows === 0){
                     return res.status(500).json({
-                        "Error": "Server Internal Error"
+                        "Error": "Não foi possivel atualizar item"
                     })
                 }else{
                     return res.status(200).json({
